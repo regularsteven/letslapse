828ddd1fe0b941735048947ffc07229cbbd989bc
Steven Wright <hello@regularsteven.com>
2021-04-25 14:02:04 +0700

    testing on device

--- timelapse.py
+++ timelapse.py
@@ -3,10 +3,10 @@ from os import system
 
 camera = PiCamera()
 
-for i in range(30):
+for i in range(200):
     print(i)
     camera.capture('demo/image{0:04d}.jpg'.format(i))
 
-system('ffmpeg -framerate 10 -pattern_type glob -i "demo/*.jpg" -c:v libx264 -crf 0 demo/output.mp4')
+system('ffmpeg -framerate 25 -pattern_type glob -i "demo/*.jpg" -c:v libx264 -crf 0 demo/output.mp4')
 
 print('done')
