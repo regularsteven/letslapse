8fee67b5daa8e24f73b26018649a57435eb95b1d
Steven Wright <hello@regularsteven.com>
2021-04-25 14:14:24 +0700

    testing on device

--- timelapse.py
+++ timelapse.py
@@ -1,11 +1,16 @@
 from picamera import PiCamera
 from os import system
+from time import sleep
+
 
 camera = PiCamera()
+#full resolution = 4056, 3040
+camera.resolution = (4056, 3040)
 
 for i in range(200):
     print(i)
     camera.capture('demo/image{0:04d}.jpg'.format(i))
+    sleep(6)
 
 system('ffmpeg -framerate 25 -pattern_type glob -i "demo/*.jpg" -c:v libx264 -crf 0 demo/output.mp4')
 
