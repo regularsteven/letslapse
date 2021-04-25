dda5c09abc8046f228901f7e0752cbe7dd9ac298
Steven Wright <hello@regularsteven.com>
2021-04-25 15:52:01 +0700

    updated for raw

--- timelapse.py
+++ timelapse.py
@@ -1,17 +1,32 @@
-from picamera import PiCamera
+#from picamera import PiCamera
 from os import system
 from time import sleep
+from pydng.core import RPICAM2DNG
 
 
-camera = PiCamera()
+#camera = PiCamera()
 #full resolution = 4056, 3040
-camera.resolution = (4056, 3040)
+#camera.resolution = (4056, 3040)
 
-for i in range(200):
-    print(i)
-    camera.capture('demo/image{0:04d}.jpg'.format(i))
-    sleep(6)
+#sleep(2)
+#camera.capture("demo/rawtest.yuv", 'yuv')
+#sleep(2)
+d=RPICAM2DNG()
+#d.convert("demo/rawyuv.jpg")
 
-system('ffmpeg -framerate 25 -pattern_type glob -i "demo/*.jpg" -c:v libx264 -crf 0 demo/output.mp4')
+
+for i in range(50):
+     print(i)
+     system("raspistill -r -o demo/testing_{0:04d}.jpg".format(i))
+     d.convert("demo/testing_{0:04d}.jpg".format(i))
+     sleep(1)
+
+
+#for i in range(200):
+#    print(i)
+#    camera.capture('demo/image{0:04d}.jpg'.format(i))
+#    sleep(6)
+
+#system('ffmpeg -framerate 25 -pattern_type glob -i "demo/*.jpg" -c:v libx264 -crf 0 demo/output.mp4')
 
 print('done')
