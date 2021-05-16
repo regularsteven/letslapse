b6626ccca0f5bf08b4ec4cdb1ebf9088fc27d23f
Steven Wright <hello@regularsteven.com>
2021-05-17 06:22:52 +0700

    work in progress, building capability to support grouping by image-numbers or images by timespan, not finished still in progress

--- longexposure.py
+++ longexposure.py
@@ -1,42 +1,53 @@
 import os, numpy, PIL
-from PIL import Image
+from PIL import Image, ExifTags, ImageStat
 from os import system
+import time
 import argparse
+from datetime import datetime
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--imagesToBatch', help='number of images to batch')
+parser.add_argument('--groupBy', help='number to group batching by with --type images or seconds')
+parser.add_argument('--groupByType', help='images or seconds - group images as per --groupBy')
+parser.add_argument('--makeMP4', help='images or seconds - group images as per --groupBy')
 args = parser.parse_args()
 
 
 
-system("rm blendedImage*")
+#system("rm blendedImage*")
 
 #### Access all JPG files in directory
 allfiles=os.listdir(os.getcwd())
 print("allfiles")
-print(allfiles)
+#print(allfiles)
 
 #imlist=[filename for filename in allfiles if  filename[-4:] in [".jpg",".JPG"]]
 
 
+def getMeta ( filename ):
+    img = Image.open(filename)
+    exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
+    imageDateTime = exif["DateTimeDigitized"]
+    d = datetime.strptime(imageDateTime, "%Y:%m:%d %H:%M:%S")
+    #dawnRamp = datetime.fromisoformat(d)
+    return d.timestamp()
+    
+
+
 #['image0.jpg', 'image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg']
 
-imagesToBatch = int(args.imagesToBatch)
+groupByType = args.groupByType #images or seconds
+    #if images, this would be a simple group up by images I.e. 10 images and then merge the average pixels of all
+    #if seconds, this would require analysis of all images taken betwee the range of seconds (eg 60 seconds) and for many images taken in 60 seconds, bundle up to 1, for 2 images, bundle to one
+
+imagesToBatch = int(args.groupBy)
 fullImageSet = len(allfiles) / imagesToBatch
-for a in range(int(fullImageSet)):
-    imlist=[]
-    for i in range(imagesToBatch):
-        #print(i)
-        if a == 0:
-            imlist.append('image'+str(i)+'.jpg')
-        else :
-            imlist.append('image'+str(i+(imagesToBatch*a))+'.jpg')
+#fullImageSet = 3
+startingTimestamp = 0
 
 
-    print("imlist")
-    print(imlist)
 
+def blendGroupToOne(imlist, sequenceNo) :
     #### Assuming all images are the same size, get dimensions of first image
     w,h=Image.open(imlist[0]).size
     N=len(imlist)
@@ -53,9 +64,49 @@ for a in range(int(fullImageSet)):
 
     #### Generate, save and preview final image
     out=Image.fromarray(arr,mode="RGB")
-    fileName = "blendedImage"+str(a)+".jpg"
+    fileName = "blendedImage"+str(sequenceNo)+".jpg"
     print(fileName)
     out.save(fileName)
     #out.show()
 
-system("ffmpeg -i blendedImage%d.jpg -b:v 100000k -vcodec mpeg4 -r 25 a_blendedVideo"+str(imagesToBatch)+".mp4")
+if groupByType == "images" :
+    for a in range(int(fullImageSet)):
+        imlist=[]
+        for i in range(imagesToBatch):
+            #print(i)
+            if a == 0:
+                imlist.append('image'+str(i)+'.jpg')
+            else :
+                imlist.append('image'+str(i+(imagesToBatch*a))+'.jpg')
+        print("imlist")
+        print(imlist)
+
+        blendGroupToOne(imlist, a)
+else :
+    print("building up lists of images within certain timeranges")
+    timerangeGroupIndex = 0
+    imlist=[]
+    for a in range(int(fullImageSet)):
+        
+        filename = 'image'+str(a)+'.jpg'
+        thisTimestamp = getMeta( filename )
+        print(thisTimestamp)
+        
+        if a == 0 : 
+            startingTimestamp = thisTimestamp
+
+
+        if(thisTimestamp < (startingTimestamp + imagesToBatch)) :
+            imlist.append(filename)
+        else: 
+            
+            blendGroupToOne(imlist, timerangeGroupIndex)
+            timerangeGroupIndex = timerangeGroupIndex+1
+            startingTimestamp = thisTimestamp
+            imlist=[]
+
+
+
+
+if args.makeMP4 == "yes" :
+    system("ffmpeg -i blendedImage%d.jpg -b:v 100000k -vcodec mpeg4 -r 25 a_blendedVideo"+str(imagesToBatch)+".mp4")
