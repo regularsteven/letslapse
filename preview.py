88bde1f3e154976a2e9a617e45205c263c09198c
Steven Wright <hello@regularsteven.com>
2021-05-22 11:24:48 +0700

    typo fix

--- preview.py
+++ preview.py
@@ -6,7 +6,7 @@ from decimal import Decimal
 import argparse
 
 #example usage:
-#ppython3 preview.py --ss 1000 --iso 100 --awbg 3,2
+#python3 preview.py --ss 1000 --iso 100 --awbg 3,2
 
 # Instantiate the parser
 parser = argparse.ArgumentParser(description='Optional app description')
