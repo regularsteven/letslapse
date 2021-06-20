3d5098e7b31482099727a3d4c6fe55f26efc3107
Steven Wright <hello@regularsteven.com>
2021-06-21 04:45:05 +0700

    basic function to evaluate video for awbg settings for potential presets

---
+++ awbg-capture.py
@@ -0,0 +1,16 @@
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera(resolution=(1280, 720), framerate=30)
+# Set ISO and meter mode to the desired value
+camera.iso = 400
+camera.meter_mode = 'matrix'
+# Wait for the automatic gain control to settle
+sleep(2)
+# Now fix the values
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+g = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = g
+print("Captured AWB at %r", g)
