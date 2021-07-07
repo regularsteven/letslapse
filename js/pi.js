376803410fd7eaf7ff558b2b305e2d38f39f26d9
Steven Wright <hello@regularsteven.com>
2021-07-07 15:32:00 +0200

    added basic adjustment for localhost development with a pi on the network for a stream to play with

--- js/pi.js
+++ js/pi.js
@@ -44,7 +44,12 @@ function toggleControls(value){
 function streamManager(startOrStop){
     if(startOrStop == "start"){
         document.getElementById("imageViewport").style.backgroundImage = "none";
-        stream = "http://10.3.141.212:8081/stream.mjpg"
+        stream = ""
+        if(window.location.host == "127.0.0.1"){
+            console.log("local testing, see if the device is on the network")
+            stream += "http://10.3.141.212"
+        }
+        stream = "/stream.mjpg"
         document.getElementById("imageViewport").style.backgroundImage = "url('"+stream+"')";
     }else{
         window.stop();
