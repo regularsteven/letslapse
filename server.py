0945bea688059e707ebc301deb165650774c9c73
Steven Wright <hello@regularsteven.com>
2021-07-07 20:02:47 +0200

    added call for threaded streamer script

--- server.py
+++ server.py
@@ -93,6 +93,8 @@ class StreamingOutput(object):
         return self.buffer.write(buf)
 
 class MyHttpRequestHandler(server.BaseHTTPRequestHandler):
+    processThread = threading.Thread(target=thread_second)
+    processThread.start()
     def do_GET(self):
         print(urlparse(self.path))
         query_components = parse_qs(urlparse(self.path).query)
