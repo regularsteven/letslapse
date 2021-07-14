b1cca0a3c2e18a63251962e29ca9222badb65004
Steven Wright <hello@regularsteven.com>
2021-07-14 07:02:11 +0200

    added new function to make calling the right subfunction better

--- js/pi.js
+++ js/pi.js
@@ -514,6 +514,16 @@ function displayStatus(which){
 }
 
 
+function openGallery(){
+
+    //$(".stillGalleryBtn").removeClass("active");
+    if($(".timelapseGalleryBtn").hasClass("active")){
+        getShoots();
+    }else{
+        getStills();
+    }
+}
+
 
 function getShoots(filter){
     var apiCall = "/?action=getShoots";
@@ -559,26 +569,22 @@ function displayShoots(gallery){
 
 function getStills(){
 
-    //$(".stillGalleryBtn").removeClass("active");
-    if($(".timelapseGalleryBtn").hasClass("active")){
-        getShoots();
-    }else{
 
 
-        var apiCall = "/?action=getStills";
-        $.getJSON( apiCall)
-        .done(function( json ) {
-            console.log( "JSON Data: ");
-            console.log(json);
-            //log("getStills: "+ json.stills);
-            displayStills(json.stills);
-        })
-        .fail(function( jqxhr, textStatus, error ) {
-            var err = textStatus + ", " + error;
-            console.log( "Request Failed: " + err );
-            log("getStills ERROR: "+ error)
-        });
-    }
+    var apiCall = "/?action=getStills";
+    $.getJSON( apiCall)
+    .done(function( json ) {
+        console.log( "JSON Data: ");
+        console.log(json);
+        //log("getStills: "+ json.stills);
+        displayStills(json.stills);
+    })
+    .fail(function( jqxhr, textStatus, error ) {
+        var err = textStatus + ", " + error;
+        console.log( "Request Failed: " + err );
+        log("getStills ERROR: "+ error)
+    });
+
 
 }
 var stillsApp = null;
