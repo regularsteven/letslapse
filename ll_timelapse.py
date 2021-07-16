a4d2934a9cb3186c68b9b728552709230fe38fa0
Steven Wright <hello@regularsteven.com>
2021-07-16 22:01:01 +0200

    swapped append and write values

--- ll_timelapse.py
+++ ll_timelapse.py
@@ -61,9 +61,9 @@ print(args)
 def storeProgress (index, folder,shutterSpeed, DG, AG, blueGains, redGains, raw, nightMode):
     filename = "timelapse_"+str(folder)+"/timelapse.log"
     if path.isfile(filename) == False:
-        f = open(filename, "a")
-    else:
         f = open(filename, "w")
+    else:
+        f = open(filename, "a")
     f.write("image"+str(index)+".jpg,"+str(shutterSpeed)+","+str(DG)+","+str(AG)+","+str(blueGains)+","+str(redGains)+","+str(raw)+","+str(nightMode))
     f.close()
     system("echo '"+str(index)+"\n"+folder+"\n"+str(float(shutterSpeed))+"\n"+str(DG)+"\n"+str(AG)+"\n"+str(blueGains)+"\n"+str(redGains)+"\n"+str(raw)+"\n"+str(nightMode)+"' >progress.txt")
