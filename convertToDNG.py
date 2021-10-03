e9d462c4133b3a2f76a639ee6aa323b7cc5f2b4e
Steven Wright <hello@regularsteven.com>
2021-10-03 15:09:08 +0200

    added exclusion for thumbnail images

--- convertToDNG.py
+++ convertToDNG.py
@@ -3,6 +3,9 @@ import os
 
 import argparse
 
+#usage 
+#python3 /home/steven/Documents/dev/letslapse/convertToDNG.py 
+
 # Instantiate the parser
 parser = argparse.ArgumentParser(description='Optional app description')
 
@@ -16,7 +19,8 @@ print(args.pos_arg)
 def files(path):
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
-            if ".jpg" in file:
+            if ".jpg" in file and "_thumb" not in file:
+                print(file)
                 yield os.path.join(path, file)
 
 for file in files(args.pos_arg+"/"):
