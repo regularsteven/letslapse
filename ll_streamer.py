519cd54636d6e9d4baefea5641a154b8739b452b
Steven Wright <hello@regularsteven.com>
2021-10-15 12:38:12 +0200

    steps to allow for testing and webcam use on desktop for end to end concept development without a pi and pi camera

--- ll_streamer.py
+++ ll_streamer.py
@@ -19,9 +19,17 @@ import sys
 from urllib.parse import urlparse, parse_qs
 import argparse
 
-PORT = 8081
+PORT = 8083
 
-import picamera
+import ll_utils
+
+if ll_utils.isPi() == True:
+    import picamera
+else:
+    print("running on a non-pi")
+    import cv2
+    #import requests
+    import numpy as np
 
 class StreamingOutput(object):
     def __init__(self):
@@ -40,29 +48,62 @@ class StreamingOutput(object):
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
+
         if 'stream.mjpg' in self.path:
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    print("streaming now")
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
+            if ll_utils.isPi() == False:
+                self.send_response(200)
+                self.send_header('Age', 0)
+                self.send_header('Cache-Control', 'no-cache, private')
+                self.send_header('Pragma', 'no-cache')
+                self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+                self.end_headers()
+                try:
+                    while True:
+                        #with output.condition:
+                        #    output.condition.wait()
+                        
+                        cap = cv2.VideoCapture(1)
+                        
+                        ret, frame = cap.read()
+                        
+                        _, jpg = cv2.imencode(".jpg", frame)
+
+                        self.wfile.write(b'--FRAME\r\n')
+                        self.send_header('Content-Type', 'image/jpeg')
+                        self.send_header('Content-Length', len(frame))
+                        self.end_headers()
+                        self.wfile.write(jpg)
+                        print("streaming now")
+                        self.wfile.write(b'\r\n')
+                        #self.wfile.write(jpg)
+
+                except Exception as e:
+                    logging.warning(
+                        'Removed streaming client %s: %s',self.client_address, str(e))
+                        
+            else: 
+                self.send_response(200)
+                self.send_header('Age', 0)
+                self.send_header('Cache-Control', 'no-cache, private')
+                self.send_header('Pragma', 'no-cache')
+                self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+                self.end_headers()
+                try:
+                    while True:
+                        with output.condition:
+                            output.condition.wait()
+                            frame = output.frame
+                        self.wfile.write(b'--FRAME\r\n')
+                        self.send_header('Content-Type', 'image/jpeg')
+                        self.send_header('Content-Length', len(frame))
+                        self.end_headers()
+                        self.wfile.write(frame)
+                        print("streaming now")
+                        self.wfile.write(b'\r\n')
+                except Exception as e:
+                    logging.warning(
+                        'Removed streaming client %s: %s',
+                        self.client_address, str(e))
         
         else:
             self.send_error(404)
@@ -72,18 +113,27 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
-with picamera.PiCamera(resolution='640x480', framerate=15) as camera:
-    output = StreamingOutput()
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = ('', PORT)
-        server = StreamingServer(address, StreamingHandler)
-        print("Streaming.")
-        server.serve_forever()
+if ll_utils.isPi() == True:
+    with picamera.PiCamera(resolution='640x480', framerate=15) as camera:
+        output = StreamingOutput()
+        camera.start_recording(output, format='mjpeg')
+        try:
+            address = ('', PORT)
+            server = StreamingServer(address, StreamingHandler)
+            print("Streaming.")
+            server.serve_forever()
 
-    finally:
-        print("ERROR: Stream not able to run. Stream ended.")
-        camera.stop_recording()
+        finally:
+            print("ERROR: Stream not able to run. Stream ended.")
+            camera.stop_recording()
+else: 
+    with socketserver.TCPServer(("", PORT), StreamingHandler) as httpd:
+        print("Serving at port ", PORT)
+        try:
+            httpd.serve_forever()
+        except:
+            pass
+
 
 # print in the command line instead of file's console
 if __name__ == '__main__':
