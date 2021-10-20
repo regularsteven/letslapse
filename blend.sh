eee2b82b19f13976ad981fb970c4471d0920613d
Steven Wright <hello@regularsteven.com>
2021-10-20 19:41:09 +0200

    added input for export of range for faster processing, export to camera folder now optional at end

--- blend.sh
+++ blend.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-INPUT_FILE=$2
 PROCESS_SPEED=$1
+INPUT_FILE=$2
 
+EXPORT_FROM_TIME=$3
+EXPORT_TO_TIME=$4
+
+FFMPEG_EXPORT=" "
+
+if [ -n "$EXPORT_FROM_TIME" ]; then
+    if [ -n "$EXPORT_TO_TIME" ]; then
+        FFMPEG_EXPORT="-ss $EXPORT_FROM_TIME -to $EXPORT_TO_TIME "
+    fi
+fi
 
 basename "$INPUT_FILE"
 FILE_NAME_EXTN="$(basename -- $INPUT_FILE)"
@@ -17,53 +27,157 @@ echo "1 Video to Image Sequence"
 echo " - $INPUT_FILE at $PROCESS_SPEED x speed to IMAGES"
 
 
-#echo $FILE_NAME_EXTN
-#echo $FILE_NAME
-#echo $FILE_EXTN
+POST_PROCESS=0
+
+if [ $PROCESS_SPEED -gt 10 ]; then
+    echo "------------------------------"
+    echo "------------------------------"
+    echo "Big process speed, need to break into smaller tasks"
+    echo "------------------------------"
+    echo "------------------------------"
+    
+    if [ $PROCESS_SPEED == 20 ]; then
+        PROCESS_SPEED=5
+        POST_PROCESS=4
+    fi
+
+    if [ $PROCESS_SPEED == 25 ]; then
+        PROCESS_SPEED=5
+        POST_PROCESS=5
+    fi
+
+    if [ $PROCESS_SPEED == 30 ]; then
+        PROCESS_SPEED=6
+        POST_PROCESS=5
+    fi
+    if [ $PROCESS_SPEED == 40 ]; then
+        PROCESS_SPEED=10
+        POST_PROCESS=4
+    fi
+    if [ $PROCESS_SPEED == 50 ]; then
+        PROCESS_SPEED=10
+        POST_PROCESS=5
+    fi
+    if [ $PROCESS_SPEED == 100 ]; then
+        PROCESS_SPEED=10
+        POST_PROCESS=10
+    fi
+fi
 
 FRAMES_DIR="videos/$FILE_NAME-frames"
-STACKED_FOLDER="videos/$FILE_NAME-stacked-$PROCESS_SPEED-images"
-
 mkdir "$SCRIPT_DIR/$FRAMES_DIR"
+ffmpeg $FFMPEG_EXPORT-i $INPUT_FILE -qscale:v 1 $FRAMES_DIR/image%d.jpg
 
-ffmpeg -i $INPUT_FILE -qscale:v 1 $FRAMES_DIR/image%d.jpg
 
 
+
+STACKED_FOLDER="videos/$FILE_NAME-stacked-$PROCESS_SPEED-images"
 mkdir "$SCRIPT_DIR/$STACKED_FOLDER"
-
-echo "2 Blend & Stack Images"
-
-max=$PROCESS_SPEED
 TOTAL_IMAGES=$(ls $FRAMES_DIR/ | wc -l)
+TOTAL_IMAGES_MATCH_PROCESS_SPEED=`expr $TOTAL_IMAGES  %  $PROCESS_SPEED`
 
-#SAFE is to ensure we don't try to add more images into a frame than we've added in other images - so it might cut short a little
-TOTAL_IMAGES_SAFE=`expr $TOTAL_IMAGES - $PROCESS_SPEED`
+TOTAL_IMAGES_SAFE=$TOTAL_IMAGES
 
+if [ $TOTAL_IMAGES_MATCH_PROCESS_SPEED != 0 ]; then
+    TOTAL_IMAGES_SAFE=`expr $TOTAL_IMAGES  -  $PROCESS_SPEED`
+fi
 
 THIS_FILE=0
 INPUT_IMAGE=0
-for imageSet in `seq 1 $TOTAL_IMAGES`; do
-    THIS_OUTPUT_FILE=`expr $THIS_OUTPUT_FILE + 1`
-    FILES_TO_CONVERT=""
-    #echo "imageSet : $imageSet"
-    FROM_IMAGE=$INPUT_IMAGE
-    for image in `seq 1 $max`; do
-        INPUT_IMAGE=`expr $INPUT_IMAGE + 1`
-        #CUR_INDEX=`expr $image \* $imageSet`
-        #echo " :$INPUT_IMAGE"
-        FILES_TO_CONVERT+="$FRAMES_DIR/image$INPUT_IMAGE.jpg "
+
+echo "2 Blend & Stack Images"
+
+while [ $POST_PROCESS -ge 0 ]; do
+    
+    echo "POST PROCESS: $POST_PROCESS"
+    
+    for imageSet in `seq 1 $TOTAL_IMAGES`; do
+        THIS_OUTPUT_FILE=`expr $THIS_OUTPUT_FILE + 1`
+        FILES_TO_CONVERT=""
+        #echo "imageSet : $imageSet"
+        FROM_IMAGE=`expr $INPUT_IMAGE + 1`
+        for image in `seq 1 $PROCESS_SPEED`; do
+            INPUT_IMAGE=`expr $INPUT_IMAGE + 1`
+            #CUR_INDEX=`expr $image \* $imageSet`
+            #echo " :$INPUT_IMAGE"
+            FILES_TO_CONVERT+="$FRAMES_DIR/image$INPUT_IMAGE.jpg "
+        done
+
+        echo "blending image$FROM_IMAGE.jpg to image$INPUT_IMAGE.jpg into image${THIS_OUTPUT_FILE}.jpg"
+        convert $FILES_TO_CONVERT-evaluate-sequence mean $STACKED_FOLDER/image${THIS_OUTPUT_FILE}.jpg
+
+        rm $FILES_TO_CONVERT
+        if [ $INPUT_IMAGE -ge $TOTAL_IMAGES_SAFE ]
+        then
+            rm $FRAMES_DIR -R
+            echo "No more to process"
+            break
+        fi
     done
-    echo "blending image$FROM_IMAGE.jpg to image$INPUT_IMAGE.jpg into img${THIS_OUTPUT_FILE}.jpg"
-    convert $FILES_TO_CONVERT-evaluate-sequence mean $STACKED_FOLDER/img${THIS_OUTPUT_FILE}.jpg
-    if [ $INPUT_IMAGE -ge $TOTAL_IMAGES_SAFE ]
-    then
-        echo "No more files to process"
-        break
+
+    if [ $POST_PROCESS == 0 ]; then
+        echo "NO MORE"
+        PROCESS_SPEED=$1
+        POST_PROCESS=-1
     fi
+
+    if [ $POST_PROCESS -gt 0 ]; then
+        
+        echo "ONE MORE BATCH"
+        
+
+        THIS_FILE=0
+        INPUT_IMAGE=0
+        THIS_OUTPUT_FILE=0
+
+        TOTAL_IMAGES=$(ls $STACKED_FOLDER/ | wc -l)
+        
+        FRAMES_DIR=$STACKED_FOLDER
+
+
+        STACKED_FOLDER="videos/$FILE_NAME-stacked-$1-images"
+
+        mkdir "$SCRIPT_DIR/$STACKED_FOLDER"
+
+        
+        PROCESS_SPEED=$POST_PROCESS
+        
+        
+        
+        TOTAL_IMAGES_MATCH_PROCESS_SPEED=`expr $TOTAL_IMAGES  %  $PROCESS_SPEED`
+
+        TOTAL_IMAGES_SAFE=$TOTAL_IMAGES
+
+        if [ $TOTAL_IMAGES_MATCH_PROCESS_SPEED != 0 ]; then
+            TOTAL_IMAGES_SAFE=`expr $TOTAL_IMAGES  -  $PROCESS_SPEED`
+        fi
+        echo $TOTAL_IMAGES_SAFE
+        POST_PROCESS=0
+
+    fi
+
+
 done
 
 
+ffmpeg -i $STACKED_FOLDER/image%d.jpg -b:v 100000k -vcodec mpeg4 -r 25 videos/$FILE_NAME-$PROCESS_SPEED-speed.mp4
 
-ffmpeg -i $STACKED_FOLDER/img%d.jpg -b:v 100000k -vcodec mpeg4 -r 25 videos/$FILE_NAME-$PROCESS_SPEED-speed.mp4
+
+while true; do
+    read -p "Move video to Camera folder?" yn
+    case $yn in
+        [Yy]* ) mv videos/$FILE_NAME-$PROCESS_SPEED-speed.mp4 ../../dcim/Camera/; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 
 
+while true; do
+    read -p "Move stacked images to Camera folder?" yn
+    case $yn in
+        [Yy]* ) mv $STACKED_FOLDER ../../dcim/Camera/; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
