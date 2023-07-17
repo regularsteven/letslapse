a60a2de6084054fffcdadbf38d7128b21ee39b63
Steven Wright <hello@regularsteven.com>
2023-07-17 11:47:20 +0100

    testing raw

--- camera/camera.py
+++ camera/camera.py
@@ -1,11 +1,15 @@
 from picamera2 import Picamera2, Preview
 import time
 picam2 = Picamera2()
-#picam2.start_and_capture_file("filename.jpg",delay=0,show_preview=False)
-
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
 picam2.start_preview(Preview.DRM)
+
+
+
+preview_config = picam2.create_preview_configuration()
+capture_config = picam2.create_still_configuration(raw={})
+picam2.configure(preview_config)
+
 picam2.start()
 time.sleep(2)
-picam2.capture_file("test2.jpg")
+
+picam2.switch_mode_and_capture_file(capture_config, "full.dng", name="raw")
