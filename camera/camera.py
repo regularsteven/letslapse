f99a1465419f0142da1387c1a71746942c499d9c
Steven Wright <hello@regularsteven.com>
2023-07-18 15:01:40 +0100

    basic loop in place which runs pretty slow

--- camera/camera.py
+++ camera/camera.py
@@ -17,21 +17,31 @@ picam2 = Picamera2()
 picam2.start_preview(Preview.DRM)
 
 
-filename = args.filename + "_" + str(args.lensPosition) + "." + args.format
-
-preview_config = picam2.create_preview_configuration()
-capture_config = picam2.create_still_configuration(raw={}, display=None)
-picam2.configure(preview_config)
-picam2.start()
-picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": args.lensPosition})
-time.sleep(2)
-
-r = picam2.switch_mode_capture_request_and_stop(capture_config)
-
-if args.format == "jpg":
-    r.save("main", filename)
-else:
-    r.save_dng(filename)
 
 
 
+def capture_and_save_image(index):
+    filename = args.filename + "_" + str(args.lensPosition) + "_" + str(index) +  "." + args.format
+
+    preview_config = picam2.create_preview_configuration()
+    capture_config = picam2.create_still_configuration(raw={}, display=None)
+    picam2.configure(preview_config)
+    picam2.start()
+    picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": args.lensPosition})
+    time.sleep(2)
+
+    r = picam2.switch_mode_capture_request_and_stop(capture_config)
+
+    if args.format == "jpg":
+        r.save("main", filename)
+    else:
+        r.save_dng(filename)
+
+    picam2.stop()
+
+
+
+#create a loop to capture 10 images and save them as an image sequence with a 1 second delay between each image
+for i in range(1):
+    time.sleep(1)
+    capture_and_save_image(i)
