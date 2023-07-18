69212d516b58797b53559a49f3b76a77c51d25ae
Steven Wright <hello@regularsteven.com>
2023-07-18 06:34:38 +0100

    testing on device capture

--- camera/camera.py
+++ camera/camera.py
@@ -5,9 +5,9 @@ import time
 
 parser = argparse.ArgumentParser(description='Optional params')
 
+parser.add_argument('--filename', type=str, help='file name', default="image")
 parser.add_argument('--format', type=str, help='image format', default="jpg")
 parser.add_argument('--lensPosition', type=float, help='Macro 15 to Infinate 2.75', default=2.75)
-parser.add_argument('--name', type=str, help='image name', default="image")
 
 
 args = parser.parse_args()
@@ -18,19 +18,22 @@ picam2.start_preview(Preview.DRM)
 
 
 #preview_config = picam2.create_preview_configuration()
+picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": args.lensPosition})
+
+#time.sleep(2)
 
 if args.format == "jpg":
     capture_config = picam2.create_still_configuration()
 else:
-    capture_config = picam2.create_still_configuration(raw={})
+    capture_config = picam2.create_still_configuration(raw={}, display=None)
+    picam2.start()
+    r = picam2.switch_mode_capture_request_and_stop(capture_config)
+    #r.save("main", "full.jpg")
+    r.save_dng("full.dng")
+    #picam2.switch_mode_and_capture_file(capture_config, "full.dng", name="raw")
 
 
-picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": args.lensPosition})
 
 #picam2.configure(preview_config)
 
 #picam2.start(show_preview=False)
-time.sleep(2)
-
-filename = args.name + "_" + str(args.lensPosition) +  "." + args.format
-picam2.switch_mode_and_capture_file(capture_config, filename, name=args.format)
